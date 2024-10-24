438ead5a4cf216ec979ac5f5050b61490a6044d6
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-23 23:05:36 -0600

    borrando

--- src/main/java/controllers/CustomerController.java
+++ src/main/java/controllers/CustomerController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
- * @author axelmaya
- */
+
 
 @RestController
 public class CustomerController {
