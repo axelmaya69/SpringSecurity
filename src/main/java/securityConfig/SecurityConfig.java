548e1eab149217ece856a6a36e343bfd6132136b
axelmaya69 <axeldbmaya96@gmail.com>
2025-01-01 22:51:32 -0600

    agregando parametros

--- src/main/java/securityConfig/SecurityConfig.java
+++ src/main/java/securityConfig/SecurityConfig.java
@@ -1,8 +1,22 @@
 package securityConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/index").permitAll() // Permitir acceso público a /index
+                        .anyRequest().authenticated()         // Proteger las demás rutas
+                )
 
 
 }
